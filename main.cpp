63abb546fe6fcc0b937b8aba8d10b10f4443cb82
Javier <javier.fuentesm@hotmail.com>
2020-02-23 20:26:58 -0600

    Ejercicio 4

--- main.cpp
+++ main.cpp
@@ -1,31 +1,34 @@
 #include <iostream>
 #include <unistd.h>
 #include <thread>
+#include <atomic>
 using namespace std;
+
 int valor = 0;
+atomic<int> globalv(0);
 
 void disminuye() {
-    for (int i = 0; i < 100000; i++) {
-        valor -= 1;
-        //printf("Hilo %ld ejecutando disminucion(). Valor: %d\n", this_thread::get_id(), valor);
+    for (int i = 0; i < 10000; i++) {
+        globalv -= 1;
+        //cout << "Hilo: " << this_thread::get_id() << " ejecutando disminucion() el valor es= " << globalv << endl;
         //sleep(1);
     }
 }
 
 void aumenta() {
-    for (int i = 0; i < 100000; i++) {
-        valor += 1;
-        //printf("Hilo %ld ejecutando aumento(). Valor: %d\n", this_thread::get_id(), valor);
+    for (int i = 0; i < 10000; i++) {
+        globalv += 1;
+        //cout << "Hilo: " << this_thread::get_id() << " ejecutando aumento() el valor es= " << globalv << endl;
         //sleep(1);
     }
 }
 
 int main() {
-    thread th1(aumenta), th2(disminuye);
+    thread th1(disminuye), th2(aumenta);
     printf("Proceso principal espera que los hilos terminen\n");
     th1.join();
     th2.join();
-    printf("El hilo principal termina\n");
-    printf("VALOR: %d\n", valor);
+    cout << "El hilo principal termina\n";
+    cout << "el valor es" << globalv.load() << endl;
     exit(0);
 }
