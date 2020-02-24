e724683a5e78e8958c235aeb94886ff78bfad29e
Javier <javier.fuentesm@hotmail.com>
2020-02-23 20:38:32 -0600

    Ejercicio 6

--- main.cpp
+++ main.cpp
@@ -1,35 +1,41 @@
 #include <iostream>
 #include <unistd.h>
 #include <thread>
-#include <atomic>
+#include <mutex>
 using namespace std;
-
 int valor = 0;
-atomic<int> globalv(0);
+mutex m;
 
-void disminuye(atomic<int>& globalv) {
+void disminuye() {
     for (int i = 0; i < 100000; i++) {
-        globalv -= 1;
-        //printf("Hilo %ld ejecutando aumento(). Valor: %d\n", this_thread::get_id(), globalv.load());
+        m.lock();
+
+        valor -= 1;
+        //printf("Hilo %ld ejecutando disminucion(). Valor: %d\n", this_thread::get_id(), valor);
         //sleep(1);
+
+        m.unlock();
     }
 }
 
-void aumenta(atomic<int>& globalv) {
+void aumenta() {
     for (int i = 0; i < 100000; i++) {
-        globalv += 1;
+        m.lock();
 
-        //printf("Hilo %ld ejecutando aumento(). Valor: %d\n", this_thread::get_id(), globalv.load());
+        valor += 1;
+        //printf("Hilo %ld ejecutando aumento(). Valor: %d\n", this_thread::get_id(), valor);
         //sleep(1);
+        m.unlock();
+
     }
 }
 
 int main() {
-    thread th1(disminuye, ref(globalv)), th2(aumenta, ref(globalv));
+    thread th1(aumenta), th2(disminuye);
     printf("Proceso principal espera que los hilos terminen\n");
     th1.join();
     th2.join();
-    cout << "El hilo principal termina\n";
-    cout << "el valor es" << globalv.load() << endl;
+    printf("El hilo principal termina\n");
+    printf("VALOR: %d\n", valor);
     exit(0);
 }
