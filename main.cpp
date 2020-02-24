3b0abe11009b0bdd986e5a50e13b49e69877854a
Javier <javier.fuentesm@hotmail.com>
2020-02-23 20:32:54 -0600

    Ejercicio 5

--- main.cpp
+++ main.cpp
@@ -7,24 +7,25 @@ using namespace std;
 int valor = 0;
 atomic<int> globalv(0);
 
-void disminuye() {
-    for (int i = 0; i < 10000; i++) {
+void disminuye(atomic<int>& globalv) {
+    for (int i = 0; i < 100000; i++) {
         globalv -= 1;
-        //cout << "Hilo: " << this_thread::get_id() << " ejecutando disminucion() el valor es= " << globalv << endl;
+        //printf("Hilo %ld ejecutando aumento(). Valor: %d\n", this_thread::get_id(), globalv.load());
         //sleep(1);
     }
 }
 
-void aumenta() {
-    for (int i = 0; i < 10000; i++) {
+void aumenta(atomic<int>& globalv) {
+    for (int i = 0; i < 100000; i++) {
         globalv += 1;
-        //cout << "Hilo: " << this_thread::get_id() << " ejecutando aumento() el valor es= " << globalv << endl;
+
+        //printf("Hilo %ld ejecutando aumento(). Valor: %d\n", this_thread::get_id(), globalv.load());
         //sleep(1);
     }
 }
 
 int main() {
-    thread th1(disminuye), th2(aumenta);
+    thread th1(disminuye, ref(globalv)), th2(aumenta, ref(globalv));
     printf("Proceso principal espera que los hilos terminen\n");
     th1.join();
     th2.join();
