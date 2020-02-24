cd9f54f7079642e008ec2353035d32d61e406691
Javier <javier.fuentesm@hotmail.com>
2020-02-23 20:07:46 -0600

    Primer Ejercicio

---
+++ cmake-build-debug/CMakeFiles/practica8.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/practica8.dir/main.cpp.o"
+  "practica8"
+  "practica8.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/practica8.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
